5ba4a90b4e0c90cf5f35f3dd1898620dfe432e79
gregorydhughes <gregorydhughes@gmail.com>
2016-09-11 16:15:17 -0400

    Added some new stuff!

---
+++ ReflexAgent/ReflexAgent/PerceptRec.h
@@ -0,0 +1,19 @@
+#ifndef PERCEPTREC_H
+#define PERCEPTREC_H
+
+struct PerceptRec
+{
+	_int8 touch;
+	_int8 dUnder;
+	_int8 dFront;
+	_int8 dBack;
+	_int8 dLeft;
+	_int8 dRight;
+	_int8 gUnder;
+	_int8 gFront;
+	_int8 gBack;
+	_int8 gLeft;
+	_int8 gRight;
+};
+
+#endif // !PERCEPTREC_H
---
+++ ReflexAgent/ReflexAgent/RoomClass.cpp
@@ -0,0 +1,24 @@
+#include "RoomClass.h"
+
+RoomClass::RoomClass()
+{
+}
+
+PerceptRec RoomClass::GetPercepts(LocRec loc)
+{
+	PerceptRec ans;
+
+	// Check Touch
+
+	// Check under
+
+	// Check front
+
+	// Check Back
+
+	// Check Left
+
+	// Check Right
+
+	return ans;
+}
