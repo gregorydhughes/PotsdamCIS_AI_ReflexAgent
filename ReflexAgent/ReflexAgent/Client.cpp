c9d6f04841ca85b9d36f4ac369e173e8a7c4a7d9
GarryG <valepos@gmail.com>
2016-09-11 17:53:28 -0400

    Constructed initial agent location start

--- ReflexAgent/ReflexAgent/Client.cpp
+++ ReflexAgent/ReflexAgent/Client.cpp
@@ -5,40 +5,70 @@
 
 using namespace std;
 
+// Parameters: &rc - room object to be constructed and filled
+//             with stuff
 // Post-Condition: Builds room for agent to explore and clean
-void buildRoom() {
-	buildRoom();
+void buildRoom(RoomClass &rc);
+
+int main() {
+	// Initialize room object
+	RoomClass rc;
+	buildRoom(rc);
+
+	// Set continue conditions
+	int moves = rc.getRoomSize() * rc.getRoomSize();
+	bool goal = false;
+
+	// Set start location
+	LocRec currentLocation;
+	currentLocation.x = 0;
+	currentLocation.y = 0;
+
+	Direction dir = NORTH;
+
+	// Clean room
+	while (moves > 0 && !goal) {
+		PerceptRec tempRec = rc.GetPercepts(currentLocation);
+		
+
+		moves--;
+	}
+}
+
+// Parameters: &rc - room object to be constructed and filled
+//             with stuff
+// Post-Condition: Builds room for agent to explore and clean
+void buildRoom(RoomClass &rc) {
+	// Read in from file
 	cout << "Please enter file name: " << endl;
 	string file;
 	cin >> file;
 	ifstream fin;
 	fin.open(file.c_str());
 
+	// Set values from file;
 	int n, numberOfFurniture, numberOfDirtPiles;
 	fin >> n >> numberOfFurniture >> numberOfDirtPiles;
+
+	// Build room
+	rc = RoomClass(n);
+
+	// Set goal
 	LocRec goal;
 	fin >> goal.x >> goal.y;
+	rc.SetGoalOnLocation(goal);
 
-	RoomClass rc(n);
-
+	// Place furniture
 	for (int i = 0; i < numberOfFurniture; i++) {
 		LocRec temp;
 		fin >> temp.x >> temp.y;
 		rc.SetFurnitureOnLocation(temp);
 	}
 
+	// Place dirt
 	for (int i = 0; i < numberOfDirtPiles; i++) {
 		LocRec temp;
 		fin >> temp.x >> temp.y;
 		rc.SetDirtOnLocation(temp);
-	}
-}
-
-int main() {
-
-	
-
-	while (moves > 0 && !goal) {
-		
 	}
 }
